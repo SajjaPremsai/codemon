788b35ba38f43e3af286608dbed484e9df08cd09
Sajja Prem Sai <sajjapremsai8938@gmail.com>
2025-04-15 16:32:31 +0000

    Changed the input taking function and added the readline for accessing the previous executed funtions

--- Main.c
+++ Main.c
@@ -1,51 +1,93 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<string.h>
-#include<unistd.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "List.h"
+#include "DisplayFiles.h"
 
-void main(){
-    char input[256];
-    char command[256], args[256];
-
-    char * cwd = getcwd(NULL, 0);
+int main() {
+    char input[256]; 
+    char **args = malloc(10 * sizeof(char *)); 
+    if (!args) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    char *cwd = getcwd(NULL, 0);    
     if (cwd == NULL) {
         perror("getcwd failed");
         exit(EXIT_FAILURE);
     }
 
-    while(1){
-        printf("%s </> ", cwd); 
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = '\0';  
-
-        int args_count = sscanf(input, "%s %s", command, args);
-        if (args_count == 0) {
-            continue;  // Ignore empty input
-        }
-
-        if (strcmp(command, "ls") == 0) {
-            list_directories(cwd);  
-        }
-
-        if (strcmp(command, "cd") == 0) {
-            if (chdir(args) == 0) {
-                cwd = getcwd(NULL, 0);
-            } else {
-                perror("cd failed");
-            }
-        }
-
-        if (strcmp(command, "clear") == 0) {
-            system("clear");  
-        }
-
-        if (strcmp(command, "exit") == 0) {
+    while (1) {
+        char prompt[512];
+        snprintf(prompt, sizeof(prompt), "%s </> ", cwd);
+        char *input = readline(prompt);
+        if (input == NULL) {
             break; 
         }
+
+        if (strlen(input) > 0) {
+            add_history(input); 
+        }
+        input[strcspn(input, "\n")] = '\0';
+
+        int i = 0;
+        args[i] = strtok(input, " ");
+        
+        while (args[i] != NULL) {
+            i++;
+            
+            if (i >= 10) {
+                args = realloc(args, (i + 1) * sizeof(char *)); 
+                if (!args) {
+                    perror("realloc failed");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            args[i] = strtok(NULL, " ");
+        }
+
+        if (args[0] == NULL) {
+            continue;
+        }
+
+        if (strcmp(args[0], "ls") == 0) {
+            list_directories(cwd);
+        }
+        else if (strcmp(args[0], "cd") == 0) {
+            if (args[1] != NULL) {
+                if (chdir(args[1]) == 0) {
+                    cwd = getcwd(NULL, 0);
+                } else {
+                    perror("cd failed");
+                }
+            } else {
+                printf("Usage: cd <directory>\n");
+            }
+        }
+        else if (strcmp(args[0], "cat") == 0) {
+            if (args[1] != NULL) {
+                cat_command(args[1]);
+            } else {
+                printf("Usage: cat <filename>\n");
+            }
+        }
+        else if (strcmp(args[0], "clear") == 0) {
+            system("clear");
+        }
+        else if (strcmp(args[0], "exit") == 0) {
+            break;
+        }
+        else {
+            printf("%s: command not found\n", args[0]);
+        }
     }
+
+    free(args);
+    free(cwd);
+    return 0;
 }
